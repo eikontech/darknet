4f03949496275885d75da191078c6a14919f5d79
Simone Nardi <simone.nardi@eikontech.it>
2022-09-05 18:47:25 +0200

    from unsigned to signed int

--- include/yolo_v2_class.hpp
+++ include/yolo_v2_class.hpp
@@ -28,13 +28,13 @@ struct center_t
 
 struct point_t
 {
-    unsigned int x, y;
-    point_t(unsigned int x, unsigned int y) : x(x), y(y) {}
+    int x, y;
+    point_t(int x, int y) : x(x), y(y) {}
 };
 
 struct bbox_t
 {
-    unsigned int x, y, w, h;     // (x,y) - top-left corner, (w, h) - width & height of bounded box
+    int x, y, w, h;     // (x,y) - top-left corner, (w, h) - width & height of bounded box
     float prob;                  // confidence - probability that the object was found correctly
     unsigned int obj_id;         // class of object - from range [0, classes-1]
     unsigned int track_id;       // tracking id for video (0 - untracked, 1 - inf - tracked object)
